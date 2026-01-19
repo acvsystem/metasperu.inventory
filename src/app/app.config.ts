ea9f4b19f02d8f62e66bb96b5ccb4e370feb2b7e
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-01-19 11:18:58 -0500

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,7 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately'
     })
   ]
 };
