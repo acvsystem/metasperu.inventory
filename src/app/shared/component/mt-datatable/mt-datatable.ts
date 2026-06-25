f7b4115990c49057603ca5733742f326d3a871ee
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-06-25 11:34:54 -0500

    UPDATE

--- src/app/shared/component/mt-datatable/mt-datatable.ts
+++ src/app/shared/component/mt-datatable/mt-datatable.ts
@@ -112,7 +112,7 @@ export class MtDatatable implements OnInit, OnChanges, AfterViewInit {
               if (opcion === 'positivo') return valorNumerico >= 1 && valorNumerico != data['cStock'];
               if (opcion === 'negativo') return valorNumerico < 0;
               if (opcion === 'cero sin escaneo') return data['cConteo'] == 0;
-              if (opcion === 'cero escaneo') return (data['cConteo'] > 0 && data['cTotalConteo'] == 0) || data['cTotalConteo'] == data['cStock'];
+              if (opcion === 'cero escaneo') return (data['cConteo'] == 0 && data['cTotalConteo'] == 0) ;
               return false;
             });
           }
